600c203a61513f4dc8026da5a02ccb7cf14a50a1
Saniya Khan <140809875+saniya0911@users.noreply.github.com>
2024-02-17 20:33:46 +0530

    Time: 2 ms (23.49%), Space: 42 MB (75.44%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -11,8 +11,12 @@ class Solution {
     }
     int gcd(int a,int b)
     {
-        if(b==0)
-            return a;
-        return gcd(b,a%b);
+        int g =1;
+        for(int i =1;i<=Math.min(a,b);i++)
+        {
+            if(a%i==0 && b%i==0)
+                g = Math.max(g,i);
+        }
+        return g;
     }
 }
