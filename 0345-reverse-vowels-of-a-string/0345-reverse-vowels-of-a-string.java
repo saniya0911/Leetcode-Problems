a779aa8b3aa65c75f66c92de781205aeb82813d0
Saniya Khan <140809875+saniya0911@users.noreply.github.com>
2026-07-09 17:58:18 +0530

    Time: 3 ms (72.58%), Space: 46.6 MB (65.32%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,31 +1,53 @@
 class Solution {
     public String reverseVowels(String s) {
-        ArrayList<Character> vowels = new ArrayList<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
-        String vow = "";
-        String ans = "";
-        int n = s.length();
-        for(int i = 0; i<n ;i++)
+        // ArrayList<Character> vowels = new ArrayList<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        // String vow = "";
+        // String ans = "";
+        // int n = s.length();
+        // for(int i = 0; i<n ;i++)
+        // {
+        //     char ch = s.charAt(i);
+        //     if(vowels.contains(Character.toLowerCase(ch)))
+        //     {
+        //         vow += ch;
+        //     }
+        // }
+        // int j = vow.length() -1;
+        // for(int i =0 ;i< n;i++)
+        // {
+        //     char ch = s.charAt(i);
+        //     if(vowels.contains(Character.toLowerCase(ch)) && j>=0)
+        //     {
+        //         ans += vow.charAt(j);
+        //         j--;
+        //     }
+        //     else
+        //     {
+        //         ans += ch;
+        //     }
+        // }
+        // return ans;
+        char ch[] = s.toCharArray();
+        String vow= "aeiouAEIOU";
+        int low = 0;
+        int high = s.length() -1;
+        while(low < high)
         {
-            char ch = s.charAt(i);
-            if(vowels.contains(Character.toLowerCase(ch)))
+            while(low < high && vow.indexOf(ch[low]) == -1)
             {
-                vow += ch;
+                low++;
             }
+            while(low < high && vow.indexOf(ch[high]) == -1)
+            {
+                high--;
+            }
+            char temp = ch[low];
+            ch[low] = ch[high];
+            ch[high] = temp;
+            low ++;
+            high --;
         }
-        int j = vow.length() -1;
-        for(int i =0 ;i< n;i++)
-        {
-            char ch = s.charAt(i);
-            if(vowels.contains(Character.toLowerCase(ch)) && j>=0)
-            {
-                ans += vow.charAt(j);
-                j--;
-            }
-            else
-            {
-                ans += ch;
-            }
-        }
+        String ans = new String(ch);
         return ans;
     }
 }
