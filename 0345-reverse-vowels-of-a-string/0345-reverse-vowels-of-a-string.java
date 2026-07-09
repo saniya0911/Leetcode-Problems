f0de70422c9942eebb8c55b2222deacc708f38b1
Saniya Khan <140809875+saniya0911@users.noreply.github.com>
2026-07-09 17:49:31 +0530

    Time: 282 ms (5.08%), Space: 48.4 MB (5.72%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,27 +1,31 @@
 class Solution {
     public String reverseVowels(String s) {
-        String upper = "AEIOU";
-        String lower = "aeiou";
-        StringBuilder vowels=new StringBuilder();
+        ArrayList<Character> vowels = new ArrayList<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        String vow = "";
+        String ans = "";
         int n = s.length();
-        for(int i=0;i<n;i++)
+        for(int i = 0; i<n ;i++)
         {
-            char ch=s.charAt(i);
-            if(upper.indexOf(ch)==-1 && lower.indexOf(ch)==-1)
-                continue;
-            else
-                vowels.append(ch);
+            char ch = s.charAt(i);
+            if(vowels.contains(Character.toLowerCase(ch)))
+            {
+                vow += ch;
+            }
         }
-        vowels.reverse();
-        int k =0;
-        for(int i=0;i<n;i++)
+        int j = vow.length() -1;
+        for(int i =0 ;i< n;i++)
         {
-            char ch=s.charAt(i);
-            if(upper.indexOf(ch)==-1 && lower.indexOf(ch)==-1)
-                continue;
+            char ch = s.charAt(i);
+            if(vowels.contains(Character.toLowerCase(ch)) && j>=0)
+            {
+                ans += vow.charAt(j);
+                j--;
+            }
             else
-                s=s.substring(0,i)+vowels.charAt(k++)+s.substring(i+1,n);
+            {
+                ans += ch;
+            }
         }
-        return s;
+        return ans;
     }
 }
