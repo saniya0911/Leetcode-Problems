ee95978a06611c149b30584d794476ca6eefbf54
Saniya Khan <140809875+saniya0911@users.noreply.github.com>
2026-08-22 20:29:28 +0530

    Time: 2 ms (76.74%), Space: 19.3 MB (52.93%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,17 +1,30 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [True] * (len(s)+1)
-        return self.issegment(0, s, wordDict, dp)
+    #     dp = [True] * (len(s)+1)
+    #     return self.issegment(0, s, wordDict, dp)
     
-    def issegment(self, start, s, wordDict, dp):
-        if not dp[start]:
-            return False
-        if(start == len(s)):
-            return True
+    # def issegment(self, start, s, wordDict, dp):
+    #     if not dp[start]:
+    #         return False
+    #     if(start == len(s)):
+    #         return True
         
-        for i in range(start+1, len(s)+1):
-            if s[start: i] in wordDict and self.issegment(i, s, wordDict, dp):
-                return True
+    #     for i in range(start+1, len(s)+1):
+    #         if s[start: i] in wordDict and self.issegment(i, s, wordDict, dp):
+    #             return True
 
-        dp[start] = False
-        return False
+    #     dp[start] = False
+        # return False
+
+        dp = [False] * (len(s)+1)
+        dp[0] = True
+
+        for i in range(0, len(s)):
+            if not dp[i]:
+                continue
+            
+            for word in wordDict:
+                size = len(word)
+                if i+size <= len(s) and s[i:i+size] == word:
+                    dp[i+size] = True
+        return dp[len(s)]
