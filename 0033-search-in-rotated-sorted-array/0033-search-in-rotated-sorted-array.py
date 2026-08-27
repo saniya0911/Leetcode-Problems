48105341f993f827f5e5333e94affdb8b1b313a4
Saniya Khan <140809875+saniya0911@users.noreply.github.com>
2026-08-27 23:56:12 +0530

    Time: 0 ms (100%), Space: 19.5 MB (38.86%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,42 +1,5 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # n = len(nums)
-        # ans = -1
-        # left = 0
-        # right = n-1
-        # while(left <= right):
-        #     mid = (left+right)//2
-        #     if nums[mid] == target:
-        #         return mid
-        #     elif nums[mid] > nums[right]:
-        #         # if nums[mid] < target:
-        #             if nums[left] <= target:
-        #                 right = mid - 1
-        #             elif nums[left] > target:
-        #                 left = mid + 1
-        #         # else:
-        #             # if nums[left] < target:
-        #             #     right = mid - 1
-        #             # elif nums[left] > target:
-        #             #     left = mid + 1
-        #     elif nums[mid] < nums[right]:
-        #         if nums[left] < nums[mid]:
-        #             if nums[mid] > target:
-        #                 right = mid - 1
-        #             else:
-        #                 left = mid + 1
-        #         else:
-        #             if nums[mid] >= target:
-        #                 left = mid +1 
-        #             else:
-        #                 right = mid - 1
-        #     # elif nums[mid] > target:
-        #     #     right = mid - 1
-        #     # else:
-        #     #     left = mid + 1
-        #     elif left == right:
-        #         break
-        # return ans
         n = len(nums)
         left = 0
         right = n - 1
