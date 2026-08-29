8ce3c9280d33997c7eebe028cafed3256f27efeb
Saniya Khan <140809875+saniya0911@users.noreply.github.com>
2026-08-29 19:59:19 +0530

    Time: 11 ms (79.1%), Space: 19.6 MB (32.79%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,12 +1,13 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n = len(nums)
+        # n = len(nums)
         # dp = [1] * n
         # for i in range(1, n):
         #     for j in range(i):
         #         if nums[i] > nums[j]:
         #             dp[i] = max(dp[i], dp[j]+1)
         # return max(dp)
+        n = len(nums)
         sub = []
         for i in range(n):
             if not sub or sub[-1] < nums[i]:
@@ -30,3 +31,14 @@ class Solution:
                 index = mid
                 right = mid - 1
         return index
+        ans = self.solve(nums, 0, -inf)
+        return ans
+    # def solve(self, nums, i, prev):
+    #     if i >= len(nums):
+    #         return 0
+        
+    #     dont_take = self.solve(nums, i+1, prev)
+    #     take= 0
+    #     if nums[i] > prev:
+    #         take = 1 + self.solve(nums, i+1, nums[i])
+    #     return max(take, dont_take)
