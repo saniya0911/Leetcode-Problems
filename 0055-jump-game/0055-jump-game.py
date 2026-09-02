98e9e6b40a42e639ea2f197473a8cbfeb1995912
Saniya Khan <140809875+saniya0911@users.noreply.github.com>
2026-09-02 05:52:39 +0530

    Time: 20 ms (51.32%), Space: 20.3 MB (45.72%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -2,18 +2,31 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         if not nums:
             return True
+
+        jumps = 0
         n = len(nums)
-        dp = [False] * n
-        dp[n-1] = True
-        for i in range(n-2, -1, -1):
-            if nums[i] >= n-1 - i:
-                dp[i] = True
-            else:
-                for j in range(i+1, i + nums[i] + 1):
-                    if j >= n:
-                        break
+        for i in range(0, n):
+            if jumps < 0:
+                return False
+            elif jumps < nums[i]:
+                jumps = nums[i]
+            jumps -= 1
+
+        return True
+        # if not nums:
+        #     return True
+        # n = len(nums)
+        # dp = [False] * n
+        # dp[n-1] = True
+        # for i in range(n-2, -1, -1):
+        #     if nums[i] >= n-1 - i:
+        #         dp[i] = True
+        #     else:
+        #         for j in range(i+1, i + nums[i] + 1):
+        #             if j >= n:
+        #                 break
                     
-                    if dp[j] == True:
-                        dp[i] = True
-                        break
-        return dp[0]
+        #             if dp[j] == True:
+        #                 dp[i] = True
+        #                 break
+        # return dp[0]
