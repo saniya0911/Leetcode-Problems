b8027ab5374df5604366b4b9ba237bf3b33a6c88
Saniya Khan <140809875+saniya0911@users.noreply.github.com>
2026-09-07 06:59:04 +0530

    Time: 55 ms (16.92%), Space: 36.9 MB (6.8%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,27 +1,54 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [''] * 26
+        self.iseow = False
+        
 class Trie:
 
     def __init__(self):
-        self.trie = []
+        # self.trie = []
+        self.root = TrieNode()
 
     def insert(self, word: str) -> None:
-        self.trie.append(word)
+        # self.trie.append(word)
+        curr = self.root
+
+        for c in word:
+            if not curr.children[ord(c)-ord('a')]:
+                curr.children[ord(c)-ord('a')] = TrieNode()
+            curr = curr.children[ord(c)-ord('a')]
+        curr.iseow = True
 
     def search(self, word: str) -> bool:
-        if word in self.trie:
-            return True
-        return False
+        # if word in self.trie:
+        #     return True
+        # return False
+        curr = self.root
+        for c in word:
+            if not curr.children[ord(c)-ord('a')]:
+                return False
+            curr = curr.children[ord(c)-ord('a')]
+
+        return curr.iseow
 
     def startsWith(self, prefix: str) -> bool:
-        n = len(prefix)
-        if n == 0:
-            return True
-        for i in range(len(self.trie)):
-            s = self.trie[i]
-            word = s[0:n]
-            if word == prefix:
-                return True
+        # n = len(prefix)
+        # if n == 0:
+        #     return True
+        # for i in range(len(self.trie)):
+        #     s = self.trie[i]
+        #     word = s[0:n]
+        #     if word == prefix:
+        #         return True
 
-        return False
+        # return False
+        curr = self.root
+        for c in prefix:
+            if not curr.children[ord(c)-ord('a')]:
+                return False
+            curr = curr.children[ord(c)-ord('a')]
+
+        return True
 
 
 # Your Trie object will be instantiated and called as such:
