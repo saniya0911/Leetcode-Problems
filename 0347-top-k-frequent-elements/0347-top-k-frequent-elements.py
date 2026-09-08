97e560408ee6d12d0af6055ac18660bd408ef760
Saniya Khan <140809875+saniya0911@users.noreply.github.com>
2026-09-08 15:14:05 +0530

    Time: 13 ms (12.93%), Space: 25.2 MB (6.34%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,38 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = [[] for i in range(len(nums)+1)]
         mp = {}
         ans = []
         for n in nums:
             if n not in mp:
                 mp[n] = 0
             mp[n] += 1
-        
-        mp = sorted(mp.items(), key =  lambda x:x[1], reverse  = True)
-        for key in mp:
-            if k>0:
-                ans.append(key[0])
+
+        for key, value in mp.items():
+            freq[value].append(key)
+        print(freq)
+        for i in range(len(freq)-1, 0, -1):
+            for x in freq[i]:
+                if k<= 0:
+                    return ans
+                ans.append(x)
                 k -= 1
-            else:
-                break
+
         return ans
+                
+
+        # mp = {}
+        # ans = []
+        # for n in nums:
+        #     if n not in mp:
+        #         mp[n] = 0
+        #     mp[n] += 1
+        
+        # mp = sorted(mp.items(), key =  lambda x:x[1], reverse  = True)
+        # for key in mp:
+        #     if k>0:
+        #         ans.append(key[0])
+        #         k -= 1
+        #     else:
+        #         break
+        # return ans
