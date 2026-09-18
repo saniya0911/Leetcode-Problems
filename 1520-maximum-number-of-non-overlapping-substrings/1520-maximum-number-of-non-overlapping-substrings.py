18a72bcb2fde3b1b0b6d11f1b68415e58399e785
Saniya Khan <140809875+saniya0911@users.noreply.github.com>
2026-09-18 13:36:07 +0530

    Time: 375 ms (28.32%), Space: 20.9 MB (26.11%) - LeetHub

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.py
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.py
@@ -12,33 +12,60 @@ class Solution:
             else:
                 indices[s[i]]['end'] = i
                 indices[s[i]]['f'] += 1
-        
-        intervals = []
-        s1 = set(s)
-        for ch in s1:
+
+        right = -1
+        ans = []
+        for i in range(n):
+            ch = s[i]
             start = indices[ch]['start']
             end = indices[ch]['end']
-            valid = True
-            i = start
+            if i == start:
+                j = i
+                valid = True
+                while j <= end:
+                    curr = s[j]
+                    if indices[curr]['start'] < start:
+                        valid = False
+                        break
+                    end = max(end, indices[curr]['end'])
+                    j += 1
 
-            while i <= end:
-                curr = s[i]
-                if indices[curr]['start'] < start:
-                    valid = False
-                    break
-                
-                end = max(end, indices[curr]['end'])
-                i += 1
+                if valid:
+                    if i > right:
+                        ans.append(s[start: end+1])
+                    else:
+                        ans[-1] = s[start:end+1]
+                    right = end
             
-            if valid:
-                intervals.append([start, end])
-        
-        intervals.sort(key = lambda x: (x[1], x[1] - x[0]))
-        ans = []
-        prev_end = -1
-        for start, end in intervals:
-            if start > prev_end:
-                ans.append(s[start: end+1])
-                prev_end = end
-        
         return ans
+                    
+        
+        # intervals = []
+        # s1 = set(s)
+        # for ch in s1:
+        #     start = indices[ch]['start']
+        #     end = indices[ch]['end']
+        #     valid = True
+        #     i = start
+
+        #     while i <= end:
+        #         curr = s[i]
+        #         if indices[curr]['start'] < start:
+        #             valid = False
+        #             break
+                
+        #         end = max(end, indices[curr]['end'])
+        #         i += 1
+            
+        #     if valid:
+        #         intervals.append([start, end])
+        
+        # intervals.sort(key = lambda x: (x[1], x[1] - x[0]))
+        # ans = []
+        # prev_end = -1
+        # for start, end in intervals:
+        #     if start > prev_end:
+        #         ans.append(s[start: end+1])
+        #         prev_end = end
+        
+        # return ans
